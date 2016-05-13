379ac61e199809eb7656dfeaa1a1a9f76d588b62
478sms <478@smsfinance.ru>
2016-05-13 19:23:15 +0600

    Update hello.py

--- web/hello.py
+++ web/hello.py
@@ -3,4 +3,4 @@ import cgi
 def app(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
     qs = cgi.parse_qs(env['QUERY_STRING'])
-    return ['%s=%s<br>' % (k, qs[k][0]) for k in qs]
+    return ['%s=%s<br>' % (k, qs[k]) for k in qs]
