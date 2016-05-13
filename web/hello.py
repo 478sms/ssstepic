f4a3374c364e71e4d9ab949406af4f9dfbdb387d
478sms <478@smsfinance.ru>
2016-05-13 19:01:44 +0600

    Create hello.py

---
+++ web/hello.py
@@ -0,0 +1,6 @@
+import cgi
+
+def app(env, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    qs = cgi.parse_qs(env['QUERY_STRING'])
+    return ['%s=%s<br>' % (k, qs[k][0]) for k in qs]
